130b9682a9e6635622e120a4339017c3f40ba5c3
SoulWater <794727663@qq.com>
2015-12-22 19:24:51 +0800

    text1

--- 画画板/ViewController.m
+++ 画画板/ViewController.m
@@ -17,6 +17,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self addAction];
+    
+}
+
+-(void)addAction
+{
+    NSLog(@"atrdf");
 }
 
 - (void)didReceiveMemoryWarning {
